7cff35537435cc82a51a9ec893518e551649fcb4
Domenik Melcher <m.domenik@gmail.com>
2016-05-10 16:33:10 +0200

    changed nunit reference to 3rdPartyLibraries folder

--- TestLinkAutomation.SVN.LogAnalyzer.Test/ModulLogicTester.cs
+++ TestLinkAutomation.SVN.LogAnalyzer.Test/ModulLogicTester.cs
@@ -2,7 +2,6 @@
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 
 namespace TestLinkAutomation.SVN.LogAnalyzer.Test
