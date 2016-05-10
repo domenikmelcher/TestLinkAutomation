e0f02390f413bd3f4e5b7de2e5bc5f7549fd0734
Domenik Melcher <m.domenik@gmail.com>
2016-05-10 13:33:56 +0200

    modified the path of the test suite

---
+++ TestLinkAutomation.SVN.LogAnalyzer.Test/ModulLogicTester.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace TestLinkAutomation.SVN.LogAnalyzer.Test
+{
+    [TestFixture]
+    public class ModuleLogicTester
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
